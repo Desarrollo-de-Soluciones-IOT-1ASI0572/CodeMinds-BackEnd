6f83324356d23cf5b163f2d52e018f8ff4185030
JDu202012207 <u202012207@upc.edu.pe>
2025-05-28 15:18:31 -0500

    chore: initial commit

---
+++ src/main/java/com/codeminds/edugo/EdugoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codeminds.edugo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdugoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdugoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeminds/edugo/EdugoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeminds.edugo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdugoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
