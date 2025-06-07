bb3e53bc3d19cc402e692c46165de3e93c254b8a
MRamirez202210582 <U202210582@upc.edu.pe>
2025-06-07 00:17:55 -0500

    feat: add openapi configuration

---
+++ src/main/java/com/codeminds/edugo/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -0,0 +1,41 @@
+package com.codeminds.edugo.shared.infrastructure.documentation.openapi.configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// Swagger: http://localhost:8080/swagger-ui/index.html
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI learningPlatformOpenApi() {
+        var openApi = new OpenAPI();
+        openApi
+                .info(new Info()
+                        .title("CodeMinds API")
+                        .description("API for the endpoints to CodeMinds application")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+                ).externalDocs(new ExternalDocumentation()
+                        .description("SpringDoc OpenAPI 3.0")
+                        .url("https://springdoc.org/"));
+
+        // Add security scheme
+
+//        final String securitySchemeName = "bearerAuth";
+//
+//        openApi.addSecurityItem(new SecurityRequirement()
+//                        .addList(securitySchemeName))
+//                .components(new Components()
+//                        .addSecuritySchemes(securitySchemeName,
+//                                new SecurityScheme()
+//                                        .name(securitySchemeName)
+//                                        .type(SecurityScheme.Type.HTTP)
+//                                        .scheme("bearer")
+//                                        .bearerFormat("JWT")));
+        return openApi;
+    }
+}
