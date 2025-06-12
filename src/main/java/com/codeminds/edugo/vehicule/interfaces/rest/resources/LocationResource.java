86438b836d8bb87677a34680c7c5b30b057b5dca
Angel Cancho <u201721995@upc.edu.pe>
2025-06-12 15:35:31 -0500

    feat(vehicle-tracking): update LocationResource to use Long for id and vehicleId

--- src/main/java/com/codeminds/edugo/vehicule/interfaces/rest/resources/LocationResource.java
+++ src/main/java/com/codeminds/edugo/vehicule/interfaces/rest/resources/LocationResource.java
@@ -1,10 +1,3 @@
 package com.codeminds.edugo.vehicule.interfaces.rest.resources;
 
-public record LocationResource(
-        Integer id,
-        Integer vehicleId,
-        Double latitude,
-        Double longitude,
-        Double speed,
-        String timestamp
-) {}
+public record LocationResource(Long id, Long vehicleId, Double latitude, Double longitude, Double speed) {}
