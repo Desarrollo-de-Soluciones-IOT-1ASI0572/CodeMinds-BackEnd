fda7c3683a4ffba70bae216cd64968ffd5f0be08
Angel Cancho <u201721995@upc.edu.pe>
2025-06-12 15:36:17 -0500

    feat(vehicle-tracking): extend TrackingCommandService with additional command handlers

--- src/main/java/com/codeminds/edugo/vehicule/domain/services/TrackingCommandService.java
+++ src/main/java/com/codeminds/edugo/vehicule/domain/services/TrackingCommandService.java
@@ -1,15 +1,25 @@
 package com.codeminds.edugo.vehicule.domain.services;
 
 import com.codeminds.edugo.vehicule.domain.model.aggregates.Vehicle;
-import com.codeminds.edugo.vehicule.domain.model.commands.ReportSpeedCommand;
-import com.codeminds.edugo.vehicule.domain.model.commands.StartRouteCommand;
-import com.codeminds.edugo.vehicule.domain.model.commands.UpdateLocationCommand;
+import com.codeminds.edugo.vehicule.domain.model.commands.*;
 import com.codeminds.edugo.vehicule.domain.model.entities.Location;
+import com.codeminds.edugo.vehicule.domain.model.entities.TripStudent;
 
 import java.util.Optional;
 
 public interface TrackingCommandService {
     Optional<Vehicle> handle(StartRouteCommand command);
+
     Optional<Location> handle(UpdateLocationCommand command);
-    Optional<Boolean> handle(ReportSpeedCommand command);
+
+    void handle(EndRouteCommand command);
+
+    void handle(RegisterStudentBoardingCommand command);
+
+    void handle(RegisterStudentExitCommand command);
+
+    Optional<Vehicle> handle(CreateVehicleCommand command);
+
+    Optional<TripStudent> handle(CreateTripStudentCommand command);
+
 }
