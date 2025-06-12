4d16ae28e484449c8d86c83299bb8aef1b83f2cc
Angel Cancho <u201721995@upc.edu.pe>
2025-06-12 15:33:44 -0500

    feat(vehicle-tracking): add query classes for active trip, current location, past trips by driver, and trip students

--- src/main/java/com/codeminds/edugo/vehicule/domain/model/queries/GetActiveTripQuery.java
+++ src/main/java/com/codeminds/edugo/vehicule/domain/model/queries/GetActiveTripQuery.java
@@ -1,4 +1,3 @@
 package com.codeminds.edugo.vehicule.domain.model.queries;
 
-public record GetCurrentLocationQuery(Long vehicleId) {
-}
+public record GetActiveTripQuery(Long vehicleId) {}
--- src/main/java/com/codeminds/edugo/vehicule/domain/model/queries/getReco.java
+++ src/main/java/com/codeminds/edugo/vehicule/domain/model/queries/getReco.java
@@ -1,4 +1,4 @@
 package com.codeminds.edugo.vehicule.domain.model.queries;
 
-public record GetCurrentLocationQuery(Long vehicleId) {
+public record getReco() {
 }
