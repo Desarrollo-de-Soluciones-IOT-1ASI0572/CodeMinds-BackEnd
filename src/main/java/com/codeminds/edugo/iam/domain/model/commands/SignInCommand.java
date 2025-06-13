dbf7f23a418d68b8632232b9d0be97e52db6c143
JDu202012207 <u202012207@upc.edu.pe>
2025-06-12 22:56:35 -0500

    feat(iam): add authentication and user management endpoints with role seeding

---
+++ src/main/java/com/codeminds/edugo/iam/domain/model/commands/SignInCommand.java
@@ -0,0 +1,4 @@
+package com.codeminds.edugo.iam.domain.model.commands;
+
+public record SignInCommand(String username, String password) {
+}
---
+++ src/main/java/com/codeminds/edugo/iam/interfaces/rest/resources/AuthenticatedUserResource.java
@@ -0,0 +1,4 @@
+package com.codeminds.edugo.iam.interfaces.rest.resources;
+
+public record AuthenticatedUserResource(Long id, String username, String token) {
+}
