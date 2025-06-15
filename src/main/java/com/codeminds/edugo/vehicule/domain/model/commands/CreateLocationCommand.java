ad49fbb4421d14f1c6d27c4fca21bfa1f95846cc
Angel Cancho <u201721995@upc.edu.pe>
2025-06-14 23:06:30 -0500

    feat(vehicle-tracking): rename UpdateLocationCommand to CreateLocationCommand and add tripId for location creation

--- src/main/java/com/codeminds/edugo/vehicule/domain/model/commands/CreateLocationCommand.java
+++ src/main/java/com/codeminds/edugo/vehicule/domain/model/commands/CreateLocationCommand.java
@@ -2,13 +2,11 @@ package com.codeminds.edugo.vehicule.domain.model.commands;
 
 import java.time.LocalDateTime;
 
-public record UpdateLocationCommand(
+public record CreateLocationCommand(
         Long vehicleId,
+        Long tripId,
         double latitude,
         double longitude,
         double speed,
         LocalDateTime timestamp
 ) {}
-
-
-//public record UpdateLocationCommand(int vehicleId, double latitude, double longitude, double speed) {}
