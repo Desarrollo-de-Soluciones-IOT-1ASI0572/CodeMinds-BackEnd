c3fa8efb067b945790f991537bd9bfa063f7a86e
Angel Cancho <u201721995@upc.edu.pe>
2025-07-06 22:41:35 -0500

    refactor(assembler): update StartRouteCommandFromResourceAssembler to accept tripId directly

--- src/main/java/com/codeminds/edugo/tracking/interfaces/rest/transform/StartRouteCommandFromResourceAssembler.java
+++ src/main/java/com/codeminds/edugo/tracking/interfaces/rest/transform/StartRouteCommandFromResourceAssembler.java
@@ -1,12 +1,9 @@
 package com.codeminds.edugo.tracking.interfaces.rest.transform;
 
 import com.codeminds.edugo.tracking.domain.model.commands.StartRouteCommand;
-import com.codeminds.edugo.tracking.interfaces.rest.resources.StartRouteResource;
 
 public class StartRouteCommandFromResourceAssembler {
-    public static StartRouteCommand toCommandFromResource(StartRouteResource resource) {
-        return new StartRouteCommand(
-                resource.tripId()
-        );
+    public static StartRouteCommand toCommandFromResource(Long tripId) {
+        return new StartRouteCommand(tripId);
     }
 }
