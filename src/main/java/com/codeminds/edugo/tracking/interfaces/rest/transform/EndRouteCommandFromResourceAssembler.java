ec1d66c745d8cd4002dd0360d9010d4e0823b574
Angel Cancho <u201721995@upc.edu.pe>
2025-07-06 22:42:04 -0500

    refactor(assembler): update EndRouteCommandFromResourceAssembler to use tripId directly

--- src/main/java/com/codeminds/edugo/tracking/interfaces/rest/transform/EndRouteCommandFromResourceAssembler.java
+++ src/main/java/com/codeminds/edugo/tracking/interfaces/rest/transform/EndRouteCommandFromResourceAssembler.java
@@ -1,14 +1,13 @@
 package com.codeminds.edugo.tracking.interfaces.rest.transform;
 
 import com.codeminds.edugo.tracking.domain.model.commands.EndRouteCommand;
-import com.codeminds.edugo.tracking.interfaces.rest.resources.EndRouteResource;
 
 import java.time.LocalDateTime;
 
 public class EndRouteCommandFromResourceAssembler {
-    public static EndRouteCommand toCommandFromResource(EndRouteResource resource) {
+    public static EndRouteCommand toCommandFromResource(Long tripId) {
         return new EndRouteCommand(
-                resource.tripId(),
+                tripId,
                 LocalDateTime.now()
         );
     }
