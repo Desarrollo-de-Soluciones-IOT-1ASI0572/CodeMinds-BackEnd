e62fa9a14585c9d5f26fc8502f30d8cd32565695
Angel Cancho <u201721995@upc.edu.pe>
2025-07-11 20:18:10 -0500

    refactor(trip): simplify ActivateEmergencyCommand by removing latitude and longitude fields

--- src/main/java/com/codeminds/edugo/tracking/domain/model/commands/ActivateEmergencyCommand.java
+++ src/main/java/com/codeminds/edugo/tracking/domain/model/commands/ActivateEmergencyCommand.java
@@ -1,7 +1,5 @@
 package com.codeminds.edugo.tracking.domain.model.commands;
 
 public record ActivateEmergencyCommand(
-        Long tripId,
-        Double latitude,
-        Double longitude
+        Long tripId
 ) {}
